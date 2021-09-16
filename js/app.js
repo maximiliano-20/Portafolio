617bf89f684c95ba5ccdede6e5dc9494881af141
maximiliano-20 <estigarribiamaximiliano73@gmail.com>
2021-09-16 17:21:50 -0300

    portafolio-v1

---
+++ js/app.js
@@ -0,0 +1,21 @@
+
+
+const botonMenu = document.querySelector('.boton-menu');
+const menuMovil = document.querySelector('.menu');
+
+botonMenu.addEventListener('click', () => {
+     menuMovil.classList.toggle('activo');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
